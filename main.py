f2c8d1e4cf30b2bee381a047a27f09660fbbf032
kamiichii <berkay.kilic.ce@gmail.com>
2024-09-24 15:45:00 +0300

    Added a function to main.py that counts the number of words in farnkenstein.txt

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+def count_words():
+    word_count = 0
+    with open("books/frankenstein.txt") as f:
+        text = f.read()
+        count_of_words = len(text.split())
+        print(count_of_words)
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
-main()
+
+count_words()
 
       
       
