ecded6310be3dd49e5bf466e6b2f7e9d0ea92bfa
kamiichii <berkay.kilic.ce@gmail.com>
2024-09-24 15:59:04 +0300

    Added a function that shows count of every unique character in frankenstein.txt

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def count_unique_letters():
+    with open("books/frankenstein.txt") as f:
+        dictionary = {}
+        text =f.read()
+        text_lowered = text.lower()
+        for character in text_lowered:
+            if character in dictionary:
+                dictionary[character] += 1
+            else:
+                dictionary[character] = 1
+        print(dictionary)
+
 def count_words():
     word_count = 0
     with open("books/frankenstein.txt") as f:
@@ -5,12 +17,14 @@ def count_words():
         count_of_words = len(text.split())
         print(count_of_words)
 
-def main():
+def print_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+def main():
+    count_unique_letters()
 
-count_words()
+main()
 
       
       
